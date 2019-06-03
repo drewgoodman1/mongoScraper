b72a010c98f9b6f80dae6ce1c9e4c1c5c2c72f6b
Andrew Goodman <drewgoodman1@yahoo.com>
2019-06-03 11:01:11 -0500

    clean up some styling

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
-//var Note = require("./models/Note.js");
-//var Article = require("./models/Article.js");
 
 var PORT = process.env.PORT || 3000;
 
@@ -31,8 +29,9 @@ app.set("view engine", "handlebars");
  
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/nytimesDB", { useNewUrlParser: true });
-//mongoose.connect("mongodb://heroku_9gdgkfhb:5ucu6r92e17jkfsj4sq9vn5i43@ds261616.mlab.com:61616/heroku_9gdgkfhb");
+//mongoose.connect("mongodb://localhost/nytimesDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://heroku_9gdgkfhb:5ucu6r92e17jkfsj4sq9vn5i43@ds261616.mlab.com:61616/heroku_9gdgkfhb");
+
 // Routes
 
 ///GET request to render Handlebars pages
@@ -46,7 +45,7 @@ app.get("/", function(req, res) {
     });
 });
 
-// A GET route for scraping the nytimes website
+//scraping the nytimes website
 app.get("/scrape", function(req, res) {
     axios.get("http://www.nytimes.com/").then(function(response) {    
         var $ = cheerio.load(response.data);
@@ -73,7 +72,7 @@ app.get("/scrape", function(req, res) {
     });
 });
 
-//a GET route to render saved articles
+//route to render saved articles
 app.get("/saved", function(req, res) {
     db.Article.find({"saved": true}).populate("notes").exec(function(error, articles) {
         var hbsObject = {
@@ -83,7 +82,7 @@ app.get("/saved", function(req, res) {
     });
 });
 
-// a POST route to update the article's saved field to true
+//route to update the article's saved field to true
 app.post("/articles/save/:id", function(req, res) {
     db.Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
     .exec(function(err, doc) {
@@ -96,7 +95,7 @@ app.post("/articles/save/:id", function(req, res) {
     });
 });
 
-// a POST route to change article's saved field to false
+//route to change article's saved field to false
 app.post("/articles/delete/:id", function(req, res) {
     // Use the article id to find and update its saved boolean
     db.Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
@@ -110,20 +109,18 @@ app.post("/articles/delete/:id", function(req, res) {
     });
 });
 
-// Delete an article
+//delete an article
 app.post("/articles/delete/:id", function(req, res) {
     // Use the article id to find and update its saved boolean
     db.Article.findOneAndUpdate({ "_id": req.params.id }, {"saved": false, "notes": []})
     // Execute the above query
     .exec(function(err, doc) {
-      // Log any errors
-      if (err) {
-        console.log(err);
-      }
-      else {
-        // Or send the document to the browser
-        res.send(doc);
-      }
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(doc);
+        }
     });
 });
 
